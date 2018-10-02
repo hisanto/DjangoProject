20df8ee78a3decf187e56f9d9251867456e3acfd
hisanto <pariyarsanto@gmail.com>
2018-10-02 23:13:54 +0545

    comparing with git-master

--- cryptoc/urls.py
+++ cryptoc/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from cryptoc.views import index, about, currencies, github, contactus
+from cryptoc.views import index, about, currencies, github, contact_us
 
 urlpatterns = [
     path('', index, name='crypto_index'),
     path('about/', about, name='crypto_about'),
-    path('crypto/', currencies, name='crypto_currencies'),
-    path('contactus/', contactus, name = 'crypto_contact'),
+    path('currency/', currencies, name='crypto_currencies'),
+    path('contact/', contact_us, name = 'crypto_contact'),
     path('github/', github, name='github'),
 ]
--- cryptoc/views.py
+++ cryptoc/views.py
@@ -5,29 +5,7 @@ API_ENDPOINT = "https://api.coinmarketcap.com/v1/ticker/"
 
 
 # Create your views here.
-def currencies(request):
-    response_data = requests.get(API_ENDPOINT)
-    ctx = {
-        "data":response_data.json()
-    }
-    return render(request,template_name='cryptoc/currencies.html',context=ctx)
-
-    return render(request,
-                  template_name='cryptoc/currencies.html',
-                  context=ctx)
-
-
-def github(request):
-    response_data = requests.get("https://api.github.com/users/hisanto")
-    ctx = {
-        "data":response_data.json()
-    }
-
-    return render(request, template_name='cryptoc/git.html', context=ctx)
-
-
-
-def index(request): #always 1st param is request
+def index(request): # always 1st param is request
     data= {
         "todays_date":str(datetime.datetime.now())
     }
@@ -44,13 +22,38 @@ def about(request):
     return render(request,template_name='cryptoc/about.html', context=data)
 
 
-def contactus(request):
+def currencies(request):
+    response_data = requests.get(API_ENDPOINT)
+    ctx = {
+        "data":response_data.json()
+    }
+    return render(request,template_name='cryptoc/currencies.html',context=ctx)
+
+    return render(request,
+                  template_name='cryptoc/currencies.html',
+                  context=ctx)
+
+
+def contact_us(request):
     data ={
         "name": "Santosh Pariyar",
         "mob_no": ["98437*****","014*****"],
         "address":"Kathmandu"
     }
     return render(request, template_name='cryptoc/contactus.html',context=data)
+
+
+def github(request):
+    response_data = requests.get("https://api.github.com/users/hisanto")
+    ctx = {
+        "data":response_data.json()
+    }
+
+    return render(request, template_name='cryptoc/git.html', context=ctx)
+
+
+
+
 
 
 
