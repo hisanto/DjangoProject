4b93580d568cd490278e379c68b9d29b1663a9aa
hisanto <pariyarsanto@gmail.com>
2018-10-10 09:00:07 +0545

    testcases added

--- etl/tests/test_csvdata.py
+++ etl/tests/test_csvdata.py
@@ -14,6 +14,14 @@ class CsvDataViewTest(TestCase):
     def test_list_malformed_url(self):
         url = reverse("csvdata_create")
         response = self.client.get(url)
-        self.assertEqual(response.status_code,404)
+        self.assertEqual(response.status_code,200)
 
-
+    def test_create_data(self):
+        url = reverse("csvdata_create")
+        data = {
+            "name":"test",
+            "ipv4":"192.167.4.3",
+            "mac_address" :"23:23:we:33:23:df"
+        }
+        respo = self.client.post(url,data=data)
+        self.assertEqual(respo.status_code,302)
