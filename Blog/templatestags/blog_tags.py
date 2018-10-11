387baf20d67880e5f9098ccd6055fb700fe9b354
hisanto <pariyarsanto@gmail.com>
2018-10-11 08:38:04 +0545

    tags and filter

---
+++ Blog/templatestags/blog_tags.py
@@ -0,0 +1,23 @@
+from django import template
+from django.db.models import Count
+
+from ..models import Post
+register = template.Library()
+
+#Model to templete bridge
+@register.simple_tag
+def post_count():
+    return Post.objects.count()
+
+@register.inclusion_tag("blog/latest_posts.html")
+def get_latest(count = 3):
+    latest_post = Post.objects.order_by("-published_date")[:count]
+    return {"latest_post" : latest_post}
+
+@register.simple_tag
+def get_popular(count=3):
+    return Post.objects.annotate(total_comment = Count('comments')).order_by('-total_comment')[:count]
+
+@register.filter(name='my_filter')
+def to_upper(text):
+    return text.upper()
